2f7920bf915b875ffcc685443f9962efcec6f267
Radoslaw Olko <radoslaw.olko@gmail.com>
2024-03-24 17:08:36 +0100

    fix nops

--- src/main/java/li/cil/sedna/riscv/R5CPUNonGenerated.java
+++ src/main/java/li/cil/sedna/riscv/R5CPUNonGenerated.java
@@ -4,7 +4,10 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.annotation.Nullable;
+import javax.management.RuntimeErrorException;
 
 import it.unimi.dsi.fastutil.longs.LongSet;
 import li.cil.sedna.api.Sizes;
@@ -86,63 +89,31 @@ public class R5CPUNonGenerated extends R5CPUTemplate {
 
     protected void interpretInstruction(final MemoryMappedDevice device, int inst, long pc, int instOffset, final int instEnd, final LongSet breakpoints) {
         try{
+            int instructionSize = (inst & 0b11) == 0b11? 4: 2;
             var instructionDeclared = decoderDecisionTree.decide(inst);
             var instructionMethod = getInstructionMethod(instructionDeclared.name);
             var instructionApplied = InstructionApplication.fromOpcode(instructionDeclared, inst);
-            // this is probably third copy of args? could probably be done in a lot less inefficient way
-            var args = new ArrayList<Object>(instructionApplied.args);
-
-            for (var parameterAnnotation : instructionMethod.getParameterAnnotations()) {
-                var annotation = parameterAnnotation[0]; // assuming there's exactly one annotation per parameter
-                if (annotation.annotationType().equals(InstructionSize.class)) {
-                    int instructionSize = (inst & 0b11) == 0b11? 4: 2;
-                    args.add(instructionSize);
-                }
-                if (annotation.annotationType().equals(ProgramCounter.class)) {
-                    args.add(pc);
-                }
-            }
 
             boolean updatePC = true;
-            try{
-                var ret = instructionMethod.invoke(this, args.stream().toArray());
-                if(ret != null && ret.getClass().equals(Boolean.class)) {
-                    if((Boolean)ret && instructionApplied.name.charAt(0) == 'B') {
-                        // don't update pc on true branch
-                        updatePC = false;
-                    }
-                }
-                if (instructionApplied.name.charAt(0) == 'J') {
-                    // don't update pc on jump
-                    updatePC = false;
-                }
-                if (instructionApplied.name.equals("ECALL")
-                    || instructionApplied.name.equals("EBREAK")
-                    || instructionApplied.name.equals("MRET")
-                    || instructionApplied.name.equals("SRET")) {
-                    // don't update pc on exceptions or returns
-                    updatePC = false;
-                }
-            } catch(InvocationTargetException e) {
-                if (Error.class.isAssignableFrom(e.getCause().getClass())) {
-                    throw (Error)e.getCause();
-                } else if(RuntimeException.class.isAssignableFrom(e.getCause().getClass())) {
-                    throw (RuntimeException)e.getCause();
-                } else if(e.getCause().getClass().equals(R5IllegalInstructionException.class)) {
-                    throw (R5IllegalInstructionException)e.getCause();
-                } else {
-                    throw new Error("unhandled exception when executing instruction", e.getCause());
-                }
-            } catch(IllegalAccessException e) {
-                throw new Error("invoke failed with IllegalAccessException", e.getCause());
+            var ret = invokeInstruction(instructionApplied, instructionMethod, pc, instructionSize);
+            if(ret && instructionApplied.name.charAt(0) == 'B') {
+                // don't update pc on true branch
+                updatePC = false;
+            }
+            if (instructionApplied.name.charAt(0) == 'J') {
+                // don't update pc on jump
+                updatePC = false;
+            }
+            if (instructionApplied.name.equals("ECALL")
+                || instructionApplied.name.equals("EBREAK")
+                || instructionApplied.name.equals("MRET")
+                || instructionApplied.name.equals("SRET")) {
+                // don't update pc on exceptions or returns
+                updatePC = false;
             }
 
             if (updatePC) {
-                if((inst & 0b11) == 0b11) {
-                    this.pc += 4;
-                } else {
-                    this.pc += 2;
-                }
+                this.pc += instructionSize;
             }
         // } catch (final MemoryAccessException e) {
         //     this.pc = pc;
@@ -153,6 +124,43 @@ public class R5CPUNonGenerated extends R5CPUTemplate {
         // } catch (final R5MemoryAccessException e) {
         //     this.pc = pc;
         //     raiseException(e.getType(), e.getAddress());
+        }
+    }
+
+    boolean invokeInstruction(InstructionApplication instructionApplied, Method instructionMethod, long pc, int instructionSize) throws R5IllegalInstructionException {
+        if (instructionApplied.name.equals("NOP")) {
+            return false;
+        }
+
+        // this is probably third copy of args? could probably be done in a lot less inefficient way
+        var args = new ArrayList<Object>(instructionApplied.args);
+
+        for (var parameterAnnotation : instructionMethod.getParameterAnnotations()) {
+            var annotation = parameterAnnotation[0]; // assuming there's exactly one annotation per parameter
+            if (annotation.annotationType().equals(InstructionSize.class)) {
+                args.add(instructionSize);
+            }
+            if (annotation.annotationType().equals(ProgramCounter.class)) {
+                args.add(pc);
+            }
+        }
+
+        try{
+            var ret = instructionMethod.invoke(this, args.stream().toArray());
+            if(ret != null && ret.getClass().equals(Boolean.class)) {
+                return (Boolean)ret;
+            }
+            return false;
+        } catch(InvocationTargetException | IllegalAccessException e) {
+            if (Error.class.isAssignableFrom(e.getCause().getClass())) {
+                throw (Error)e.getCause();
+            } else if(RuntimeException.class.isAssignableFrom(e.getCause().getClass())) {
+                throw (RuntimeException)e.getCause();
+            } else if(e.getCause().getClass().equals(R5IllegalInstructionException.class)) {
+                throw (R5IllegalInstructionException)e.getCause();
+            } else {
+                throw new Error("unhandled exception when executing instruction", e.getCause());
+            }
         }
     }
 
